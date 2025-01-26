fd1d7b2a5bcfdac84a84eb87fd9c57a234995f9f
Benjamin Atelsek <40130686+kesleta@users.noreply.github.com>
2025-01-26 03:12:56 -0500

    Update vert_cross.rs

--- vert_cross.rs
+++ vert_cross.rs
@@ -1,5 +1,6 @@
-use std::collections::HashSet;
+use std::time::Instant;
 
+use rand::{rngs::SmallRng, seq::SliceRandom, SeedableRng};
 use union_find::{QuickUnionUf, Union, UnionFind, UnionResult};
 
 //Function which takes edge indicies to pairs of vertex indicies
@@ -38,43 +39,55 @@ impl Union for ComponentData {
     }
 }
 
-
 fn main(){
+    //Record start time for peformance info
+    let start_time = Instant::now();
+
     //Grid Size
     let n = 4000;
 
     //Initialize Union-Find strcture on verticies
     let mut uf = QuickUnionUf::<ComponentData>::new((n+2)*n);
 
-    //Initalize edge hashset (not used for algorithim but needed to recover the graph)
-    let mut edges = HashSet::<usize>::new();
-
+    //Initalize edge vector (not used for algorithim but needed to recover the graph)
+    let mut edges = Vec::<usize>::with_capacity((n+1)*n + n*(n-1));
+    
     //Mark the top and bottom row of verticies as connected to the top and bottom respectivly (default for those flags is false)
     (0..n).for_each(|i| {
         uf.get_mut(i).bottom = true; 
         uf.get_mut((n+1)*n+i).top = true; 
     });
 
-    //Initalize some varibles, 'i' to hold the edge index and 'a' and 'b' to hold the vertex indicies of the endpoints
-    let (mut i, mut a, mut b); (a, i) = (0, 0);
+    //Create a list o all the edges indicies in a random order
+    let mut all_edges = (0..((n+1)*n + n*(n-1))).collect::<Vec<_>>();
 
-    while !(uf.get(a).top && uf.get(a).bottom) {
-        //Generate a random edge index.
-        i = rand::random::<usize>() % ((n+1)*n + n*(n-1));
+    all_edges.shuffle(&mut SmallRng::from_entropy());
+    
+    //Loops forever until encountering a break steament
+    loop {
+        let i = all_edges.pop().expect("Can not run out of edges");
 
-        //Insert the edge index into the HashSet
-        edges.insert(i);
+        //Insert the edge index into the edges vec
+        edges.push(i);
 
         //Determine the two endpoint indicies
-        (a, b) = endpoints(n, i);
+        let (a, b) = endpoints(n, i);
         
         //Combine the two components that the endpoints lie within
         uf.union(a, b);
+
+        //Break out of the loop if the new component is connected to both the bottom and top
+        if uf.get(a).bottom && uf.get(a).top {
+            break;
+        }
     }
     
     //Remove the most recent edge added
-    edges.remove(&i);
+    edges.pop();
 
-    //Display the number of edges
-    println!("{:?}", edges.len());
+    //Display info
+    println!("Grid size: {}", n);
+    println!("Number of edges: {}", edges.len());
+    println!("Percent of edges: {:.2?}%", 100.0*(edges.len() as f64)/(((n+1)*n + n*(n-1))) as f64);
+    println!("Execution time: {:.2?}", start_time.elapsed());
 }
